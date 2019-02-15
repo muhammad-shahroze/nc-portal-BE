a821de3b646973ab6d3a96b13924ce3bf33d0b99
Muhammad S. Bilal <muhammad.shahroze@outlook.com>
2019-02-15 10:09:06 +0000

    made adjustments to endpoints file

--- endpoints.js
+++ endpoints.js
@@ -8,24 +8,48 @@ exports.endPoints = {
         'api/topics': {
           'GET /api/fetchTopic': {
             description: 'Endpoint to obtain all Topics',
-            'POST /api/postTopic': 'Endpoint to post a single Topic',
-          },
-          'api/articles': {
-            'GET /api/articles': 'Endpoint to receive all the articles',
-            'GET /api/articles/:article_id': 'Endpoint to receive an Article by its Article ID',
-            'PATCH /api/articles/:article_id': 'Endpoint to update content in an Article using Article ID',
-            'DELETE /api/articles/:article_id': 'Endpoint to delete an Article using Article ID',
-            'GET /api/articles/:article_id/comments': 'Endpoint to display comments of an Article using Article ID',
-            'POST /api/articles/:article_id/comments': 'Endpoint to post Comment in an Article using Article ID',
-          },
-          'api/comments': {
-            'PATCH /api/comments/:comment_id': 'Endpoint to update content in Comments using its Comment ID',
-            'DELETE /api/comments/:comment_id': 'Endpoint to delete a Comment using its Comment ID',
-          },
-          'api/users': {
-            'GET /api/users': 'Endpoint to obtain all Users',
-            'POST /api/users': 'Endpoint to post a Single User',
-            'GET /api/users/:username': 'Endpoint to receive a User using their Username',
+            'POST /api/postTopic': {
+              description: 'Endpoint to post a single Topic',
+            },
+            'api/articles': {
+              'GET /api/articles': {
+                description: 'Endpoint to receive all the articles',
+                'GET /api/articles/:article_id': {
+                  description: 'Endpoint to receive an Article by its Article ID',
+                  'PATCH /api/articles/:article_id': {
+                    description: 'Endpoint to update content in an Article using Article ID',
+                    'DELETE /api/articles/:article_id': {
+                      description: 'Endpoint to delete an Article using Article ID',
+                      'GET /api/articles/:article_id/comments': {
+                        description: 'Endpoint to display comments of an Article using Article ID',
+                        'POST /api/articles/:article_id/comments': {
+                          description: 'Endpoint to post Comment in an Article using Article ID',
+                        },
+                        'api/comments': {
+                          'PATCH /api/comments/:comment_id': {
+                            description: 'Endpoint to update content in Comments using its Comment ID',
+                            'DELETE /api/comments/:comment_id': {
+                              description: 'Endpoint to delete a Comment using its Comment ID',
+                            },
+                            'api/users': {
+                              'GET /api/users': {
+                                description: 'Endpoint to obtain all Users',
+                                'POST /api/users': {
+                                  description: 'Endpoint to post a Single User',
+                                  'GET /api/users/:username': {
+                                    description: 'Endpoint to receive a User using their Username',
+                                  },
+                                },
+                              },
+                            },
+                          },
+                        },
+                      },
+                    },
+                  },
+                },
+              },
+            },
           },
         },
       },
