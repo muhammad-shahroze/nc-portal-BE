d99c6efc719bdac0812c5f75e2a5de88ffce2ff4
Muhammad S. Bilal <muhammad.shahroze@outlook.com>
2019-03-04 15:53:30 +0000

    installed cors

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 
 const app = express();
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const apiRouter = require('./routes/api');
 const {
   handle400, handle404, handle422, handle500,
 } = require('./errors');
+
+app.use(cors());
 
 app.use(bodyParser.json());
 
